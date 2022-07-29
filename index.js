d5e90f75740afcf768f94a3cb8d5aaf4032b1d6f
marcosjdaniele <105455734+marcosjdaniele@users.noreply.github.com>
2022-07-29 16:56:55 -0300

    Add files via upload
    se creo nuevo js para desafio de simulador

---
+++ index.js
@@ -0,0 +1,79 @@
+let nombre = prompt("Ingrese su nombre: ")
+
+alert("¡Bienvenido " + nombre + "!")
+
+let reserva = prompt("a Qué clase desas asistir? YOGA/SPINNING/FUNCIONAL")
+
+if((reserva == "YOGA") || (reserva == "yoga")) {
+    alert("Elegiste YOGA")
+    
+    let yoga = prompt("Ingrese horario 18/19/20 hs")
+
+    while(yoga != "ESC" ){
+        switch (entrada) {
+            case "18":
+                alert("Horario reservado: 18HS");
+                break;
+            case "19":
+                alert("Horario reservado: 19HS");
+                break;
+            case "20":
+                alert("Horario reservado: 19HS");
+                break;
+            default:
+                alert("horario incorrecto")
+                break;
+        }
+        entrada = prompt("Ingrese horario 18/19/20 hs");
+    }
+
+} else if((reserva == "SPINNING") || (reserva == "spinning")) {
+    alert("Elegiste SPINNING")
+    
+    let spinning = prompt("Ingrese horario 18/20/22 hs")
+
+    while(spinning != "ESC" ){
+        switch (entrada) {
+            case "18":
+                alert("Horario reservado: 18HS");
+                break;
+            case "20":
+                alert("Horario reservado: 20HS");
+                break;
+            case "22":
+                alert("Horario reservado: 22HS");
+                break;
+            default:
+                alert("horario incorrecto")
+                break;
+        }
+        entrada = prompt("Ingrese horario 18/20/22 hs");
+    }
+
+
+} else if((reserva == "FUNCIONAL") || (reserva == "funcional")) {
+    alert("Elegiste FUNCIONAL")
+
+    let funcional = prompt("Ingrese horario 17/19/21 hs")
+
+    while(funcional != "ESC" ){
+        switch (entrada) {
+            case "17":
+                alert("Horario reservado: 17HS");
+                break;
+            case "19":
+                alert("Horario reservado: 19HS");
+                break;
+            case "21":
+                alert("Horario reservado: 21HS");
+                break;
+            default:
+                alert("horario incorrecto")
+                break;
+        }
+        entrada = prompt("Ingrese horario 17/19/21 hs");
+    }
+
+} else {
+    alert("La opción que ingresó no es válida")
+}

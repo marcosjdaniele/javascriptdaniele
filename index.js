c614f10b107e0520ac0509e0e898c92db8c3ae1d
Marcos Daniele <marcos.j.daniele@gmail.com>
2022-08-05 19:55:18 -0400

    se agregaron detalles de alert de inicio y final

--- index.js
+++ index.js
@@ -1,97 +1,16 @@
-/*let nombre = prompt("Ingrese su nombre: ")
-
-alert("¡Bienvenido " + nombre + "!")
-
-let reserva = prompt("a Qué clase desas asistir? YOGA/SPINNING/FUNCIONAL")
-
-if((reserva == "YOGA") || (reserva == "yoga")) {
-    alert("Elegiste YOGA")
-    
-    let yoga = prompt("Ingrese horario 18/19/20 hs")
-
-    while(yoga != "ESC" ){
-        switch (yoga) {
-            case "18":
-                alert("Horario reservado: 18HS");
-                break;
-            case "19":
-                alert("Horario reservado: 19HS");
-                break;
-            case "20":
-                alert("Horario reservado: 19HS");
-                break;
-            default:
-                alert("horario incorrecto")
-                break;
-        }
-        yoga = prompt("Ingrese horario 18/19/20 hs");
-    }
-
-} else if((reserva == "SPINNING") || (reserva == "spinning")) {
-    alert("Elegiste SPINNING")
-    
-    let spinning = prompt("Ingrese horario 18/20/22 hs")
-
-    while(spinning != "ESC" ){
-        switch (spinning) {
-            case "18":
-                alert("Horario reservado: 18HS");
-                break;
-            case "20":
-                alert("Horario reservado: 20HS");
-                break;
-            case "22":
-                alert("Horario reservado: 22HS");
-                break;
-            default:
-                alert("horario incorrecto")
-                break;
-        }
-        spinning = prompt("Ingrese horario 18/20/22 hs");
-    }
-
-
-} else if((reserva == "FUNCIONAL") || (reserva == "funcional")) {
-    alert("Elegiste FUNCIONAL")
-
-    let funcional = prompt("Ingrese horario 17/19/21 hs")
-
-    while(funcional != "ESC" ){
-        switch (funcional) {
-            case "17":
-                alert("Horario reservado: 17HS");
-                break;
-            case "19":
-                alert("Horario reservado: 19HS");
-                break;
-            case "21":
-                alert("Horario reservado: 21HS");
-                break;
-            default:
-                alert("horario incorrecto")
-                break;
-        }
-        funcional = prompt("Ingrese horario 17/19/21 hs");
-    }
-
-} else {
-    alert("La opción que ingresó no es válida")
-}
-*/
-
 const productos = [{
     id:'1', 
-    name:'mancuerna',
+    name:'mancuernas',
     precio: 100,
     cantidad: 0
 }, {
     id:'2', 
-    name:'barra',
+    name:'barras',
     precio: 50,
     cantidad: 0
 }, {
     id:'3', 
-    name:'colchoneta',
+    name:'colchonetas',
     precio: 20,
     cantidad: 0
 }]
@@ -101,11 +20,12 @@ let cantidad ;
 let continuar = true ;
 let elemento
 let formControl = 'SI'
+alert("¡Bienvenido a nuestra pagina de insumos deportivos!")
 
-alert(
-    "Nombre " + productos[0].name + "! \n" +
-    "Nombre " + productos[1].name + "! \n" + 
-    "Nombre " + productos[2].name + "! \n" 
+alert("Tenemos en Stock: \n" +
+    productos[0].name + "! \n" +
+    productos[1].name + "! \n" + 
+    productos[2].name + "! \n" 
     )
 
 do{
@@ -113,7 +33,7 @@ do{
     const search = productos.find(e => e.name === elemento)
     
     if (search) {
-        cantidad = prompt("Ingrese cuantos elementos quiere:")
+        cantidad = prompt("Ingrese qué cantidad del mismo desea:")
     } else{
         alert("El elemento no existe")
     }
@@ -130,6 +50,6 @@ let total = 0
 
 carrito.forEach(e => total += e.precio * e.cantidad)
 
-alert(total)
+alert("Su saldo final va a ser de: "+ total)
 
 

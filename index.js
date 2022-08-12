50f2994a2d5c44845ac1b5b234ff6a2d1dfc1b46
marcosjdaniele <marcos.j.daniele@gmail.com>
2022-08-12 15:39:45 -0300

    Se realizaron modificaciones en el ciclo

--- index.js
+++ index.js
@@ -1,55 +1,61 @@
-const productos = [{
-    id:'1', 
-    name:'mancuernas',
-    precio: 100,
-    cantidad: 0
-}, {
-    id:'2', 
-    name:'barras',
-    precio: 50,
-    cantidad: 0
-}, {
-    id:'3', 
-    name:'colchonetas',
-    precio: 20,
-    cantidad: 0
-}]
 
-const carrito = []
-let cantidad ;
-let continuar = true ;
-let elemento
-let formControl = 'SI'
-alert("¡Bienvenido a nuestra pagina de insumos deportivos!")
+const productos = [
+	{
+		id: "1",
+		name: "mancuernas",
+		precio: 100,
+		cantidad: 0,
+	},
+	{
+		id: "2",
+		name: "barras",
+		precio: 50,
+		cantidad: 0,
+	},
+	{
+		id: "3",
+		name: "colchonetas",
+		precio: 20,
+		cantidad: 0,
+	},
+];
 
-alert("Tenemos en Stock: \n" +
-    productos[0].name + "! \n" +
-    productos[1].name + "! \n" + 
-    productos[2].name + "! \n" 
-    )
+const carrito = [];
+let cantidad;
+let continuar = true;
+let elemento;
+let formControl = "SI";
+alert("¡Bienvenido a nuestra pagina de insumos deportivos!");
 
-do{
-    elemento = prompt("Ingrese que elemento busca: ")
-    const search = productos.find(e => e.name === elemento)
-    
-    if (search) {
-        cantidad = prompt("Ingrese qué cantidad del mismo desea:")
-    } else{
-        alert("El elemento no existe")
-    }
-    
-    alert("El total va a ser " + search.precio * cantidad )
+alert(
+	"Tenemos en Stock: \n" +
+		productos[0].name +
+		"! \n" +
+		productos[1].name +
+		"! \n" +
+		productos[2].name +
+		"! \n"
+);
 
-    search.cantidad = cantidad
-    carrito.push(search)
-    formControl = prompt("desea continuar? SI/NO")
-} 
-while(formControl.toLowerCase() === "si")
+do {
+	elemento = prompt("Ingrese que elemento busca: ");
+	const search = productos.find((e) => e.name === elemento);
+	console.log(search);
 
-let total = 0
+	if (search) {
+		cantidad = prompt("Ingrese qué cantidad del mismo desea:");
+		alert("El total va a ser " + search.precio * cantidad);
+		search.cantidad = cantidad;
+		carrito.push(search);
+	} else {
+		alert("El elemento no existe");
+	}
 
-carrito.forEach(e => total += e.precio * e.cantidad)
+	formControl = prompt("desea continuar? SI/NO");
+} while (formControl.toLowerCase() === "si");
 
-alert("Su saldo final va a ser de: "+ total)
+let total = 0;
 
+carrito.forEach((e) => (total += e.precio * e.cantidad));
 
+alert("Su saldo final va a ser de: " + total);

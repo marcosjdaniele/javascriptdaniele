27c76efad62bf717f3bbe1a1a6a676d87cfd1f6d
marcosjdaniele <marcos.j.daniele@gmail.com>
2022-09-01 15:28:11 -0300

    Se modificaron botones, y se agrego calculo de cantidad por precio

--- js/index.js
+++ js/index.js
@@ -18,20 +18,22 @@ class Carrito {
       imagen: producto.querySelector("img").src,
       titulo: producto.querySelector("h4").textContent,
       precio: producto.querySelector(".precio span").textContent,
+      cantidad: producto.querySelector("input").value,
       id: producto.querySelector("a").getAttribute("data-id"),
-      cantidad: 1,
     };
     this.insertarCarrito(infoProducto);
   }
 
   insertarCarrito(producto) {
     const row = document.createElement("tr");
+    const total = producto.precio * producto.cantidad;
     row.innerHTML = `
             <td>
               <img src="${producto.imagen}" width=100>
             </td>
             <td>${producto.titulo}</td>
-            <td>${producto.precio}</td>
+            <td>${producto.cantidad}</td>
+            <td>${total}</td>
             <td>
               <a href="#" class="borrar-producto fas fa-times-circle" data-id="${producto.id}"></a>
             </td>
@@ -92,12 +94,14 @@ class Carrito {
     productosLS = this.obtenerProductosLocalStorage();
     productosLS.forEach(function (producto) {
       const row = document.createElement("tr");
+      const total = producto.precio * producto.cantidad;
       row.innerHTML = `
                 <td>
                     <img src="${producto.imagen}" width=100>
                 </td>
                 <td>${producto.titulo}</td>
-                <td>${producto.precio}</td>
+                <td>${producto.cantidad}</td>
+                <td>${total}</td>
                 <td>
                     <a href="#" class="borrar-producto fas fa-times-circle" data-id="${producto.id}"></a>
                 </td>
